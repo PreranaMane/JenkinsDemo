1a1830a6ba76e04337080ef83213504ab6d6a217
HP <HP@DESKTOP-MHP166D>
2023-11-13 20:35:28 +0530

    By prerana

---
+++ GitJenPractice/src/com/Demo/Examaple.java
@@ -0,0 +1,15 @@
+package com.Demo;
+
+public class Examaple 
+{
+   public void m1()
+   {
+	 System.out.println("M1-Method");
+   }
+   public static void main(String[] args) 
+   {
+	  System.out.println("I am learn java lang");
+	  Examaple em = new Examaple();
+	  em.m1();
+   }
+}
